ae4656f8897aa3a9179f4376881576a7248846cc
Harkone22-12 <lah20914@gmail.com>
2026-04-22 05:19:57 -0400

    speed up syscall

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct usyscall *usyscall;   // con trỏ để lưu trữ vùng nhớ
 };
